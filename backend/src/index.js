d93a5a4bbe5523d003d2c3ba972e9fc1fb77808d
itsmeSanyam09 <sanyambothra6@gmail.com>
2025-02-23 19:37:29 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-app.use(cors({ origin: 'https://chat-application-s54z.onrender.com/', credentials: true }));
+app.use(cors({ origin: 'https://chat-application-fron-git-7ae200-sanyam-jains-projects-f58e30da.vercel.app//', credentials: true }));
 app.use(bodyParser.json({ limit: '50mb' })); 
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
