8b6c4be80f0dc655d91adfacf035c7a19a794f4e
itsmeSanyam09 <sanyambothra6@gmail.com>
2025-02-24 19:37:57 +0530

    Update useChatStore.js

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast';
 import {axiosInstance} from '../lib/axios.js';
 import { useAuthStore } from './useAuthStore.js';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api";
+const BASE_URL = "";
 
 export const useChatStore = create((set,get)=>({
     messages: [],
